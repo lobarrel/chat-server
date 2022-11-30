8b59600f2271f2d5798fe3fc509453908806c070
lobarrel <loribottelli@gmail.com>
2022-11-30 10:20:13 +0100

    Created main

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use tokio::net::TcpListener;
+
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (socket, addr) = listener.accept().await.unwrap();
+}
+
