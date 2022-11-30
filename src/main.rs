c67691ace86140b7cacbc180223eab6f56c64513
lobarrel <loribottelli@gmail.com>
2022-11-30 11:20:53 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use tokio::{net::TcpListener, io::{BufReader, AsyncBufReadExt, AsyncWriteExt}, s
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (tx, rx) = broadcast::channel:: <String>(10);
+    let (tx, _rx) = broadcast::channel(10);
 
     loop{
         let (mut socket, addr) = listener.accept().await.unwrap();
@@ -17,22 +17,24 @@ async fn main() {
             let (reader, mut writer) = socket.split();
 
             let mut reader = BufReader::new(reader);
-            
             let mut line = String::new();
         
             loop{
                 tokio::select! {
-                    result = reader.read_line(&mut line) =>{
+                    result = reader.read_line(&mut line) => {
                         if result.unwrap() == 0{
                             break;
                         }
-                        tx.send(line.clone()).unwrap();
+                        tx.send((line.clone(), addr)).unwrap();
                         line.clear();
                     }
                     
-                    result = rx.recv() =>{
-                        let msg = result.unwrap();
-                        writer.write_all(msg.as_bytes()).await.unwrap();
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
                     }
                 }
             }
