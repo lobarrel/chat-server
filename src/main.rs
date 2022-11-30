b67c48adf711f0bb619394ecafefcf992c90f8e2
lobarrel <loribottelli@gmail.com>
2022-11-30 11:04:40 +0100

    Tokio spawn

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,29 @@ use tokio::{net::TcpListener, io::{BufReader, AsyncBufReadExt, AsyncWriteExt}};
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, addr) = listener.accept().await.unwrap();
-    let (reader, mut writer) = socket.split();
-
-    let mut reader = BufReader::new(reader);
-    
-    let mut line = String::new();
-
     loop{
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
 
-        writer.write_all(line.as_bytes()).await.unwrap();
+        tokio::spawn(async move{
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            
+            let mut line = String::new();
+        
+            loop{
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+        
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
+
+    
+    
 
 }
